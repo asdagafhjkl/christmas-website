0006bd67a476c091c718e1bd727c6a71ae07e85f
asdagafhjkl <asdagafhjkl@gmail.com>
2025-12-17 18:52:09 +0000

    Init flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "very cool christmas website for business";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dream2nix = {
+      url = "github:nix-community/dream2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }
+  };
+
+  outputs = { self, nixpkgs, flake-utils, gitignore, dream2nix, ... }:
+    dream2nix.lib.makeFlakeOutputs {
+      systems = flake-utils.lib.defaultSystems;
+      config.projectRoot = ./.;
+      source = gitignore.lib.gitignoreSource ./.;
+    };
+}
+

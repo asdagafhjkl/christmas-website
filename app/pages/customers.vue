251a7ee933769908cf718735dc639fd86ea0933f
asdagafhjkl <asdagafhjkl@gmail.com>
2025-12-18 12:05:15 +0000

    zxcz

--- app/pages/customers.vue
+++ app/pages/customers.vue
@@ -8,7 +8,7 @@
     <UPageSection title="FAQs" description="Here are some questions we frequently get asked by our customers">
       <UCollapsible class="flex flex-col gap-2 w-full">
         <UButton
-          label="If I shoot someone will they die"
+          label="If I shoot someone will they die?"
           color="neutral"
           variant="subtle"
           trailing-icon="i-lucide-chevron-down"
@@ -21,7 +21,7 @@
       </UCollapsible>
       <UCollapsible class="flex flex-col gap-2 w-full">
         <UButton
-          label="Is it affordable"
+          label="Is it affordable?"
           color="neutral"
           variant="subtle"
           trailing-icon="i-lucide-chevron-down"
@@ -31,6 +31,34 @@
         <template #content>
           <p>Yes it is affordable as it is 99% off and it only costs £499.99</p>
           <ULink to="/buy" as="button">Buy it now!</ULink>
+        </template>
+      </UCollapsible>
+      <UCollapsible class="flex flex-col gap-2 w-full">
+        <UButton
+          label="What military operations was it used in?"
+          color="neutral"
+          variant="subtle"
+          trailing-icon="i-lucide-chevron-down"
+          block
+        />
+    
+        <template #content>
+          <p>It was and still is used in many important and critical operations. Even used by the legendary Seal Team 6.</p>
+          <ULink to="/buy" as="button">Please buy it.</ULink>
+        </template>
+      </UCollapsible>
+      <UCollapsible class="flex flex-col gap-2 w-full">
+        <UButton
+          label="Where can I buy it?"
+          color="neutral"
+          variant="subtle"
+          trailing-icon="i-lucide-chevron-down"
+          block
+        />
+    
+        <template #content>
+          <p>You can buy it on our website.</p>
+          <ULink to="/buy" as="button">Please buy it. <i>Please</i> buy it</ULink>
         </template>
       </UCollapsible>
     </UPageSection>

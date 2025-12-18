f2193a7aab6ec2d7f3c7fbd996409d4ccab1dd99
asdagafhjkl <asdagafhjkl@gmail.com>
2025-12-18 11:36:15 +0000

    dfgsdfgsdf

--- app/pages/buy.vue
+++ app/pages/buy.vue
@@ -8,26 +8,31 @@
       <NuxtImg src="/Gun.png" alt="Our amazing gun" />
     </UPageHero>
 
-    <UPricingPlans :plans="[
-      {
-        title: 'Starter',
-        description: 'Suitable for beginners.',
-        price: 'Free',
-        features: ['The gun', 'The scoop']
-      },
-      {
-        title: 'Police Training',
-        description: 'Trainning for intermediates, by the police',
-        price: '£50/mo',
-        features: ['Training course delivered by the police', '2kg of snow per month shipped to your home', 'Everything in the starter plan']
-      },
-      {
-        title: 'Seal Team 6 Training',
-        description: 'Trainning for intermediates, by the police',
-        price: '£200/mo',
-        features: ['One-on-one training provided by a member of seal team 6', '10kg of snow per month shipped to your home', 'Everything in the starter plan']
-      },
-    ]"
-    />
+    <UContainer>
+      <UPricingPlans :plans="[
+        {
+          title: 'Starter',
+          description: 'Suitable for beginners.',
+          price: 'Free',
+          features: ['The gun', 'The scoop'],
+          button: { label: 'Buy now' }, 
+        },
+        {
+          title: 'Police Training',
+          description: 'Trainning for intermediates, by the police',
+          price: '£50/mo',
+          features: ['Training course delivered by the police', '2kg of snow per month shipped to your home', 'Everything in the starter plan'],
+          button: { label: 'Register now' }, 
+        },
+        {
+          title: 'Seal Team 6 Training',
+          description: 'Trainning for intermediates, by the police',
+          price: '£200/mo',
+          features: ['One-on-one training provided by a member of seal team 6', '10kg of snow per month shipped to your home', 'Everything in the starter plan'],
+          button: { label: 'Register now' }, 
+        },
+      ]"
+      />
+    </UContainer>
   </div>
 </template>
